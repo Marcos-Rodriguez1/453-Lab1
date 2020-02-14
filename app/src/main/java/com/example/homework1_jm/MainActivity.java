e0f67b673ea0b9a1ed29ead8f7deef2084b25b9d
Marcos Rod <54653573+Marcos-Rodriguez1@users.noreply.github.com>
2020-02-14 09:10:05 -0800

    First and third page

--- app/src/main/java/com/example/homework1_jm/MainActivity.java
+++ app/src/main/java/com/example/homework1_jm/MainActivity.java
@@ -3,15 +3,27 @@ package com.example.homework1_jm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText username;
+    private EditText password;
+    private Button signUpBtn;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Does this check out
-        //okay
+
+        username = findViewById(R.id.editText_UserName);
+        password = findViewById(R.id.editText_Password);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        loginBtn = findViewById(R.id.loginBtn);
+
     }
 }
--- app/src/main/java/com/example/homework1_jm/Welcome.java
+++ app/src/main/java/com/example/homework1_jm/Welcome.java
@@ -10,5 +10,8 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+
     }
 }
