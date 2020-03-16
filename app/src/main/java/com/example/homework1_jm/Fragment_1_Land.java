c5afcfa759e65e41cdc95757cd2fd3e79358facb
Marcos Rod <54653573+Marcos-Rodriguez1@users.noreply.github.com>
2020-03-15 20:56:21 -0700

    Fragment_Set_up

---
+++ app/src/main/java/com/example/homework1_jm/Fragment_1_Land.java
@@ -0,0 +1,6 @@
+package com.example.homework1_jm;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment_1_Land extends Fragment {
+}
---
+++ app/src/main/java/com/example/homework1_jm/Fragment_2_land.java
@@ -0,0 +1,6 @@
+package com.example.homework1_jm;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment_2_land extends Fragment {
+}
