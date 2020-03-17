a616016fce1390b163fd93a32426a480c4a16570
kidjrod <thekidjrod562@gmail.com>
2020-03-16 18:13:40 -0700

    second api

--- app/src/main/java/com/example/homework1_jm/Welcome.java
+++ app/src/main/java/com/example/homework1_jm/Welcome.java
@@ -6,9 +6,14 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -30,24 +35,48 @@ import java.util.List;
 public class Welcome extends AppCompatActivity {
 //Creates the welcome screen and gets the information from the main activity
     private String TAG=Welcome.class.getSimpleName();
-    TextView name;
-    ListView lv;
+    TextView test;
 
+    Spinner spinner;
+    Spinner spinner2;
+    String Carid;
+    String modelweb;
+
+    Button mButton;
     private static String urlcars=("https://thawing-beach-68207.herokuapp.com/carmakes");
-    private static String urlmodles =("https://thawing-beach-68207.herokuapp.com/carmodelmakes/2");
-    private static String urlavailable=("https://thawing-beach-68207.herokuapp.com/carmodelmakes/2");
+    private static String urlmodles =("https://thawing-beach-68207.herokuapp.com/carmodelmakes/");
+    private static String urlavailable=("https://thawing-beach-68207.herokuapp.com/cars/10/20/92603");
     private static String urldetails=("https://thawing-beach-68207.herokuapp.com/cars/%3ccarid");
 
     ArrayList<HashMap<String,String>> carList;
+    ArrayList<HashMap<String,String>> modelList;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
         carList=new ArrayList<>();
-        lv=(ListView) findViewById(R.id.list);
+        modelList=new ArrayList<>();
+        spinner=(Spinner) findViewById(R.id.spin);
+        spinner2=(Spinner) findViewById(R.id.spin2);
+        mButton=(Button)findViewById(R.id.button);
+        test=(TextView)findViewById(R.id.test);
+        mButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Carid= spinner.getSelectedItem().toString();
+                Carid = Carid.replaceAll("[^-?0-9]+", "");
+                test.setText(urlmodles+Carid);
+                modelweb=(urlmodles+Carid);
+                new GetModel().execute();
+            }
+        });
+
         new GetCars().execute();
     }
 
@@ -102,11 +131,80 @@ public class Welcome extends AppCompatActivity {
         {
             super.onPostExecute(result);
 
-            ListAdapter adapter=new SimpleAdapter
-                    (Welcome.this,carList,
-                            R.layout.list_item,new String[]{"id","vehicle_make"},
-                            new int []{R.id.id,R.id.vehicle_make});
-            lv.setAdapter(adapter);
+            SpinnerAdapter adapter= new SimpleAdapter(Welcome.this,carList,
+                    R.layout.list_item,new String[]{"id","vehicle_make"},
+                    new int []{R.id.id,R.id.vehicle_make});
+            spinner.setAdapter(adapter);
+
         }
     }
+
+    private class GetModel extends AsyncTask<Void,Void,Void>
+    {
+        //so you put loading bar and shit here
+        @Override
+        protected void onPreExecute()
+        {
+
+        }
+        @Override
+        protected Void doInBackground (Void... arg0)
+        {
+            modelList.clear();
+
+            HttpHandler sh=new HttpHandler();
+
+            String jsonStr=sh.makeServiceCall(modelweb);
+
+            Log.e(TAG,"response from url: "+jsonStr );
+
+            if(jsonStr != null)
+            {
+                try{
+
+                    JSONArray models = new JSONArray(jsonStr);
+
+                    for(int i=0;i<models.length();i++)
+                    {
+                        JSONObject c =models.getJSONObject(i);
+
+                        String id=c.getString("id");
+                        String model=c.getString("model");
+                        String vehicle_make_id=c.getString("vehicle_make_id");
+
+                        HashMap<String,String> modelcar=new HashMap<>();
+
+                        modelcar.put("id",id);
+                        modelcar.put("model",model);
+                        modelcar.put("vehicle_make_id",vehicle_make_id);
+
+                        modelList.add(modelcar);
+                    }
+
+                    //add the exception here if that shit doesnt work
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            SpinnerAdapter adapter= new SimpleAdapter(Welcome.this,modelList,
+                    R.layout.list2_item,new String[]{"id","model","vehicle_make_id"},
+                    new int []{R.id.idmodel,R.id.model,R.id.vehicle_makeid});
+            spinner2.setAdapter(adapter);
+
+        }
+    }
+
+
+
+
+
+
+
+
 }
